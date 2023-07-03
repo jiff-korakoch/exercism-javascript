78b6641b098b1c79effd94b5807362a7a73a59b8
Korakoch Som-in <jiff.korakoch@gmail.com>
2023-07-03 05:16:52 +0000

    refactor function calculateResellPrice

--- vehicle-purchase/vehicle-purchase.js
+++ vehicle-purchase/vehicle-purchase.js
@@ -41,9 +41,9 @@ export function chooseVehicle(option1, option2) {
 export function calculateResellPrice(originalPrice, age) {
   if (age < 3) {
     return originalPrice * 0.8;
-  } else if (age > 10) {
-    return originalPrice * 0.5;
-  } else {
+  } else if (age <= 10) {
     return originalPrice * 0.7;
+  } else {
+    return originalPrice * 0.5;
   }
 }
