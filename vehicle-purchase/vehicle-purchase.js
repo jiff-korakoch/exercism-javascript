b7619958eb8f8f252aed1ec507e07e9f6cb0f503
Korakoch Som-in <jiff.korakoch@gmail.com>
2023-07-03 04:56:50 +0000

    fix style

--- vehicle-purchase/vehicle-purchase.js
+++ vehicle-purchase/vehicle-purchase.js
@@ -39,9 +39,9 @@ export function chooseVehicle(option1, option2) {
  * @returns {number} expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  if(age < 3){
-    return originalPrice * 0.8 ;
-  } else if(age > 10){
+  if (age < 3) {
+    return originalPrice * 0.8;
+  } else if (age > 10) {
     return originalPrice * 0.5;
   } else {
     return originalPrice * 0.7;
