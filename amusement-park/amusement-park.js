123d3bc607643cf30dbcd3eaef4553b22f62d7fe
Korakoch Som-in <jiff.korakoch@gmail.com>
2023-07-08 07:23:05 +0000

    create createVisitor function

--- amusement-park/amusement-park.js
+++ amusement-park/amusement-park.js
@@ -10,7 +10,7 @@
  * @returns {Visitor} the visitor that was created
  */
 export function createVisitor(name, age, ticketId) {
-  throw new Error('Please implement the createVisitor function.');
+  return { name, age, ticketId };
 }
 
 /**
