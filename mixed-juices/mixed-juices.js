45f4e841ce3442138e02db8810dbc02b1ff5112c
Korakoch Som-in <jiff.korakoch@gmail.com>
2023-07-26 12:33:01 +0000

    fix style

--- mixed-juices/mixed-juices.js
+++ mixed-juices/mixed-juices.js
@@ -12,24 +12,23 @@
  */
 export function timeToMixJuice(name) {
   let timeInMinutes = 0;
-  
-  switch(name){
+  switch (name) {
     case "Pure Strawberry Joy":
       return timeInMinutes = 0.5;
       break;
-    case "Energizer" :
+    case "Energizer":
     case "Green Garden":
       return timeInMinutes = 1.5;
       break;
-    case "Tropical Island" :
+    case "Tropical Island":
       return timeInMinutes = 3;
       break;
-    case "All or Nothing" :
+    case "All or Nothing":
       return timeInMinutes = 5;
       break;
     default:
       return timeInMinutes = 2.5;
-  };
+  }
 }
 
 /**
@@ -41,26 +40,22 @@ export function timeToMixJuice(name) {
  * @returns {number} number of limes cut
  */
 export function limesToCut(wedgesNeeded, limes) {
-  //let wedgesNeed = 25
-  //let lime = ["small","small","large","medium","small"];
   let numberLimesCut = 0; 
-  
-  while(limes.length > 0 && wedgesNeeded > 0){
-    switch(limes.shift()){
-      case "small" : 
+  while (limes.length > 0 && wedgesNeeded > 0) {
+    switch (limes.shift()) {
+      case "small":
         wedgesNeeded -= 6;
-        break;      
-        case "medium" :
+        break;
+      case "medium":
         wedgesNeeded -= 8;
         break;
-      case "large" :
+      case "large":
         wedgesNeeded -= 10;
         break;
-    } numberLimesCut = numberLimesCut + 1;
-      //console.log(wedgesNeed);
-  } return numberLimesCut;
-  
-  
+    }
+    numberLimesCut += 1;
+  }
+  return numberLimesCut;
 }
 /**
  * Determines which juices still need to be prepared after the end of the shift.
@@ -70,27 +65,24 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  //let time = 7;
-  //let order = ['Tropical Island','Energizer', 'Limetime','All or Nothing','Pure Strawberry Joy'];
-    while (timeLeft > 0){
-    switch (orders.shift()){
+  while (timeLeft > 0) {
+    switch (orders.shift()) {
       case "Pure Strawberry Joy":
-      timeLeft -= 0.5;
-      break;
-    case "Energizer" :
-    case "Green Garden":
-      timeLeft -= 1.5;
-      break;
-    case "Tropical Island" :
-      timeLeft -= 3;
-      break;
-    case "All or Nothing" :
-      timeLeft -= 5;
-      break;
-    default:
-      timeLeft -= 2.5;
-      
+        timeLeft -= 0.5;
+        break;
+      case "Energizer" :
+      case "Green Garden":
+        timeLeft -= 1.5;
+        break;
+      case "Tropical Island":
+        timeLeft -= 3;
+        break;
+      case "All or Nothing":
+        timeLeft -= 5;
+        break;
+      default:
+        timeLeft -= 2.5;
     }
-      //  console.log(timeLeft,orders);
-    }  return orders;
+  }
+  return orders;
 }
